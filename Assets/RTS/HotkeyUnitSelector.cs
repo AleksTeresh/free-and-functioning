b26075b437f2bc6f7c1f23f89ec0379441e2524d
Arsenii Kurilov <kotbegemotoo@gmail.com>
2018-03-29 01:21:56 +0300

    Implemented initial hotkey unit selection

--- Assets/RTS/HotkeyUnitSelector.cs
+++ Assets/RTS/HotkeyUnitSelector.cs
@@ -1,30 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RTS
 {
-	public static class HotkeyUnitSelector
-	{
-		public static void HandleInput() {
-			if (Input.GetButtonDown ("Select1")) {
-				Debug.Log ("Select1 pressed");
-			}
+    public static class HotkeyUnitSelector
+    {
+        private static Dictionary<string, int> HotkeyUnitIndeces = new Dictionary<string, int>()
+        {
+            { "Select1", 0 },
+            { "Select2", 1 },
+            { "Select3", 2 },
+            { "Select4", 3 },
+            { "Select5", 4 }
+        };
 
-			if (Input.GetButtonDown ("Select2")) {
-				Debug.Log ("Select2 pressed");
-			}
+        private static string[] hotkeys = new string[]
+        {
+            "Select1",
+            "Select2",
+            "Select3",
+            "Select4",
+            "Select5"
+        };
 
-			if (Input.GetButtonDown ("Select3")) {
-				Debug.Log ("Select3 pressed");
-			}
+        public static void HandleInput(Player player, HUD hud)
+        {
+            for (int i = 0; i < hotkeys.Length; i++)
+            {
+                if (Input.GetButtonDown(hotkeys[i]))
+                {
+                    SelectPlayerUnitByHotkeyIndex(player, hud, i);
 
-			if (Input.GetButtonDown ("Select4")) {
-				Debug.Log ("Select4 pressed");
-			}
+                    return;
+                }
+            }
+        }
 
-			if (Input.GetButtonDown ("Select5")) {
-				Debug.Log ("Select5 pressed");
-			}
-		}
-	}
+        private static void SelectPlayerUnitByHotkeyIndex(Player player, HUD hud, int unitIndex)
+        {
+            Units units = player.GetComponentInChildren<Units>();
+            Unit[] playerUnits = units.GetComponentsInChildren<Unit>();
+
+            if (unitIndex < playerUnits.Length)
+            {
+                ResetPlayerUnitsSelection(playerUnits, hud);
+
+                Unit unitToSelect = playerUnits[unitIndex];
+
+                player.SelectedObject = unitToSelect;
+                unitToSelect.SetSelection(true, hud.GetPlayingArea());
+            }
+
+        }
+
+        private static void ResetPlayerUnitsSelection(Unit[] units, HUD hud)
+        {
+            for (int i = 0; i < units.Length; i++)
+            {
+                units[i].SetSelection(false, hud.GetPlayingArea());
+            }
+        }
+    }
 }
