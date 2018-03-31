b387b799ef748e226e4ad0a64edd253e488d9d22
alekte <aleksandr.tereshchenko@jubic.fi>
2018-04-01 01:48:59 +0300

    Add selected unit indicator, add ability bar placeholder

---
+++ Assets/Player/HUD/SelecttionIndicator/SelectionIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionIndicator : MonoBehaviour {
+
+    public Text NameField { get; private set; }
+    public Slider HealthSlider { get; private set; }
+    public Image Avatar { get; private set; }
+
+    // Use this for initialization
+    void Start () {
+        NameField = GetComponentInChildren<Text>();
+        HealthSlider = GetComponentInChildren<Slider>();
+        Avatar = GetComponentInChildren<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
