aad01b46dcd69f16eeaa12fca6c57fb019cb523d
alekte <aleksandr.tereshchenko@jubic.fi>
2018-04-02 14:11:30 +0300

    Add Stun ability to Tanker

---
+++ Assets/AI/Scripts/Actions/StayStillAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Actions/StayStill")]
+public class StayStillAction : Action
+{
+
+    public override void Act(StateController controller)
+    {
+        StayStill(controller);
+    }
+
+    private void StayStill(StateController controller)
+    {
+        controller.unit.StopMove();
+    }
+}
---
+++ Assets/WorldObject/Statuses/Agro/AgroStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS;
+
+namespace Statuses
+{
+    public class AgroStatus : Status
+    {
+        protected override void AffectTarget()
+        {
+            if (target && target.GetStateController())
+            {
+                var targetStateController = target.GetStateController();
+
+
+                targetStateController.chaseTarget = inflicter;
+                targetStateController.TransitionToState(ResourceManager.GetAiState("Chase Idler"));
+            }
+        }
+    }
+}
+
