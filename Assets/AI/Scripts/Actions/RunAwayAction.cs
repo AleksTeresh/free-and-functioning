5bba7a566eb440381aaf0ed1957fd67970098860
alekte <aleksandr.tereshchenko@jubic.fi>
2018-04-08 09:43:21 +0300

    Improve Kiting RunAwayAction logic

--- Assets/AI/Scripts/Actions/RunAwayAction.cs
+++ Assets/AI/Scripts/Actions/RunAwayAction.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using RTS;
+using UnityEngine.AI;
 
 [CreateAssetMenu(menuName = "AI/Actions/RunAway")]
 public class RunAwayAction : Action
@@ -19,10 +20,23 @@ public class RunAwayAction : Action
         if (target)
         {
             var offset = self.transform.position - target.transform.position;
-            // run away from the target (oppiosite direction of where the target is relative to the self)
-            controller.unit.StartMove(self.destination + offset.normalized * WALK_RADIUS);
 
-            WorkManager.GetRandomDestinationPoint(self, WALK_RADIUS);
+            // run away from the target (oppiosite direction of where the target is relative to the self)
+            var destinationPoint = self.destination + offset.normalized * WALK_RADIUS;
+            // var destinationPoint = WorkManager.GetClosestPointOnNavMesh(initialDestination, "Walkable");
+            float distance = Vector3.Distance(destinationPoint, self.transform.position);
+            bool reachable = NavMesh.CalculatePath(
+                self.transform.position,
+                destinationPoint,
+                WorkManager.GetNavMeshAreaFromName("Walkable"),
+                new NavMeshPath()
+            );
+            if (Vector3.Distance(self.transform.position, destinationPoint) < 1 || !reachable)
+            {
+                destinationPoint = WorkManager.GetPerpendicularDestinationPoint(self, destinationPoint, WALK_RADIUS);
+            }
+
+            controller.unit.StartMove(destinationPoint);
         }
     }
 }
