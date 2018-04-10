258462edaac452dbd56861161cccd7edb6f94b80
alekte <aleksandr.tereshchenko@jubic.fi>
2018-04-10 13:39:29 +0300

    Add AoeAbilityWasChosenDecision, add extra condition to Single and Multi -abilityWasChosenDecsion to filter out AoeAbilities

--- Assets/AI/Scripts/Decisions/SingleAbilityWasChosenDecision.cs
+++ Assets/AI/Scripts/Decisions/SingleAbilityWasChosenDecision.cs
@@ -8,6 +8,7 @@ public class SingleAbilityWasChosenDecision : Decision
     public override bool Decide(StateController controller)
     {
         return controller.abilityToUse != null &&
+            !(controller.abilityToUse is AoeAbility) &&
             new List<Ability>(controller.unit.abilities).Contains(controller.abilityToUse);
     }
 }
