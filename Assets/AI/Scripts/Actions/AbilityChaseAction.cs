861a4f7edfe6cfeb350f88c7efaad265d2e60aad
Arsenii Kurilov <kotbegemotoo@gmail.com>
2018-04-10 11:38:01 +0300

    Fixed units to use chosen ability immediately

---
+++ Assets/AI/Scripts/Actions/AbilityChaseAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS;
+
+[CreateAssetMenu (menuName = "AI/Actions/AbilityChase")]
+public class AbilityChaseAction : Action {
+	public override void Act (StateController controller)
+	{
+		Chase(controller);
+	}
+
+	private void Chase(StateController controller)
+	{
+		Unit unit = controller.unit;
+		WorldObject chaseTarget = controller.chaseTarget;
+		if (chaseTarget && !WorkManager.ObjectCanReachTargetWithAbility(unit, controller.abilityToUse, chaseTarget.GetFogOfWarAgent()))
+		{
+			controller.unit.StartMove(chaseTarget.transform.position);
+		}
+		else
+		{
+			controller.unit.StopMove();
+		}
+	} 
+}
