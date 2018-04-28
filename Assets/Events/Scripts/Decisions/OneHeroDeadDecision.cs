0f63c0e1660b22c508a0e685f3ab4af04c07659a
Aleksandr Tereshchenko <terap98@icloud.com>
2018-04-29 01:12:24 +0300

    Add Event Decisions for cases when 1 player's unit dies and when all player's units die, add basic gameover screen, add game over conditions to the Tutorial level

---
+++ Assets/Events/Scripts/Decisions/OneHeroDeadDecision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Events
+{
+    [CreateAssetMenu(menuName = "Events/Decisions/OneHeroDead")]
+    public class OneHeroDeadDecision : Decision
+    {
+        public Unit[] heroesToStayAlive;
+
+        public override bool Decide(StateController controller)
+        {
+            var aliveHeroes = controller.player.GetUnits();
+
+            foreach (var shouldBeAliveHero in heroesToStayAlive)
+            {
+                if (!aliveHeroes.Exists(unit => unit.name == shouldBeAliveHero.name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
