244f6bff8c786ee704c69c96668a26faea413bb0
Arsenii Kurilov <kotbegemotoo@gmail.com>
2018-05-05 14:51:13 +0300

    Fixed Tanker model transform, added textures, added WaveLight VFX, added WaveCuve VFX

---
+++ Assets/Vfx/Scripts/TextureScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Gfx
+{
+    public class TextureScroller : MonoBehaviour
+    {
+        public Material scrollableMaterial;
+        public Vector2 direction = new Vector2(1, 0);
+        public float speed = 1.0f;
+
+        private Vector2 currentOffset;
+
+        private void Start()
+        {
+            currentOffset = scrollableMaterial.GetTextureOffset("_MainTex");
+        }
+
+        private void Update()
+        {
+            currentOffset += direction * speed * Time.deltaTime;
+            scrollableMaterial.SetTextureOffset("_MainTex", currentOffset);
+        }
+    }
+}
