f362616aa153a130f5d2234dd6a878062f28c2ed
Arsenii Kurilov <kotbegemotoo@gmail.com>
2018-05-19 16:03:05 +0300

    Added music themes, Eazy Sound Manager asset and initial bgm actions

---
+++ Assets/Events/Scripts/Actions/PlayBgmAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using EazyTools.SoundManager;
+
+namespace Events
+{
+    [CreateAssetMenu(menuName = "Events/Action/PlayBgm")]
+    public class PlayBgmAction : Action
+    {
+        public AudioClip bgm;
+
+        public override void Act(StateController controller)
+        {
+            if(bgm)
+            {
+                SoundManager.PlayMusic(bgm, .8f, true, false);
+            }
+        }
+    }
+}
