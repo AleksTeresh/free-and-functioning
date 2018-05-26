937cc16a13a8614d9118b6dcc8c27dc15482809b
Aleksandr Tereshchenko <terap98@icloud.com>
2018-05-26 13:54:59 +0300

    Fix attack multimode so units always chase and primarily attack a common target, fix busy-go state so that common target is being set if it was null before

---
+++ Assets/AI/Scripts/Actions/SetClosestAsTargetIfNoCurrentAction.cs
@@ -0,0 +1,31 @@
+﻿using RTS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AI
+{
+    [CreateAssetMenu(menuName = "AI/Actions/SetClosestAsTargetIfNoCurrent")]
+    public class SetClosestAsTargetIfNoCurrentAction : Action
+    {
+        public override void Act (StateController controller)
+        {
+            WorldObject controlledObject = controller.controlledObject;
+            Vector3 currentPosition = controlledObject.transform.position;
+
+            WorldObject commonTarget = controller.targetManager.SingleTarget;
+
+            if (!commonTarget)
+            {
+                WorldObject closestEnemy = WorkManager.FindNearestWorldObjectInListToPosition(controller.nearbyEnemies, currentPosition);
+
+                if (closestEnemy)
+                {
+                    controller.targetManager.SingleTarget = closestEnemy;
+                }
+            }
+        }
+    }
+}
