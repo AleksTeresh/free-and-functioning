aa00b6f4414d9db4e59ee8d06b6c7bb64727ea31
Aleksandr Tereshchenko <terap98@icloud.com>
2018-05-26 18:12:58 +0300

    Change UpperBar to be drawn using Unity Canvas

---
+++ Assets/Player/HUD/UpperBar/TextIndicator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextIndicator : MonoBehaviour {
+
+    private Image background;
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        background = GetComponentInChildren<Image>();
+        text = GetComponentInChildren<Text>();
+    }
+	
+	public void SetColor (Color color)
+    {
+        background.color = color;
+    }
+
+    public void SetSprite (Sprite sprite)
+    {
+        background.sprite = sprite;
+    }
+
+    public void SetText(string text)
+    {
+        this.text.text = text;
+    }
+}
