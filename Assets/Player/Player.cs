7502cec0c74a6f870e2f44ac84f1404e9a265ff6
Aleksandr Tereshchenko <terap98@icloud.com>
2018-06-03 19:57:01 +0300

    Reduce hit points of Hulk down to 600 on scenes where it was higher

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using RTS;
 using Abilities;
-using Dialog;
 using Persistence;
 using System.Linq;
 
